5c8d609675a69ca17f69dcdc1e8af6894981b367
Pranali G <pranalighodake22@gmail.com>
2022-12-31 01:02:01 +0530

    commit 2

--- spring-rest-api-demo/src/test/java/com/cdac/SpringRestApiDemoApplicationTests.java
+++ spring-rest-api-demo/src/test/java/com/cdac/SpringRestApiDemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.cdac;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringRestApiDemoApplicationTests {
+import com.cdac.entity.flight;
+import com.cdac.repository.FlightRepository;
 
+@SpringBootTest
+class FlightRepositoryTest {
+
+	@Autowired
+	private FlightRepository flightRepository;
+	
 	@Test
-	void contextLoads() {
+	void addFlight() {
+		flight f = new flight();
+		f.setFlightNo("SG-2124");
+		f.setFlightDate(LocalDate.of(2023, 1, 1));
+		f.setSource("Mumbai");
+		f.setDestination("Delhi");
+		f.setDepartureTime(LocalTime.of(8,30));
+		f.setArrivalTime(LocalTime.of(10, 30));
+		f.setPrice(6000);
+		
+		flightRepository.save(f);
+		
+		//asserts missing right now
 	}
 
 }
+
+
+
